7bcae7280317932566b7ae501d82965c3acef83f
Deon Poncini <dex1337@gmail.com>
2015-01-25 00:42:12 -0800

    Add unit tests for CardSelector java bindings

--- test/net/sectorsoftware/ygo/deck/CardSelectorTest.java
+++ test/net/sectorsoftware/ygo/deck/CardSelectorTest.java
@@ -11,6 +11,15 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import net.sectorsoftware.mindbw.DataTypes.Operator;
+import net.sectorsoftware.ygo.data.DataTypes.StaticCardData;
+import net.sectorsoftware.ygo.data.DataTypes.CardType;
+import net.sectorsoftware.ygo.data.DataTypes.MonsterType;
+import net.sectorsoftware.ygo.data.DataTypes.SpellType;
+import net.sectorsoftware.ygo.data.DataTypes.TrapType;
+import net.sectorsoftware.ygo.data.DataTypes.Attribute;
+import net.sectorsoftware.ygo.data.DataTypes.Type;
+
 public class CardSelectorTest
 {
     static {
@@ -56,6 +65,116 @@ public class CardSelectorTest
         List<String> clist = cardSelector.name("Black").execute();
         for (String c : clist) {
             assertTrue(c.contains("Black"));
+        }
+    }
+
+    @Test
+    public void CardType()
+    {
+        List<String> clist = cardSelector.name("Blue").cardType(
+                CardType.SPELL).execute();
+        for (String c : clist) {
+            StaticCardData scd = cardSelector.query(c);
+            assertEquals(scd.cardType, CardType.SPELL);
+        }
+    }
+
+    @Test
+    public void Attribute()
+    {
+        List<String> clist = cardSelector.name("Dragon").attribute(
+                Attribute.DARK).execute();
+        for (String c : clist) {
+            StaticCardData scd = cardSelector.query(c);
+            assertEquals(scd.attribute, Attribute.DARK);
+        }
+    }
+
+    @Test
+    public void MonsterType()
+    {
+        List<String> clist = cardSelector.name("Wind").monsterType(
+                MonsterType.XYZ).execute();
+        for (String c : clist) {
+            StaticCardData scd = cardSelector.query(c);
+            assertEquals(scd.monsterType, MonsterType.XYZ);
+        }
+    }
+
+    @Test
+    public void Type()
+    {
+        List<String> clist = cardSelector.name("saurus").type(
+                Type.DINOSAUR).execute();
+        for (String c : clist) {
+            StaticCardData scd = cardSelector.query(c);
+            assertEquals(scd.type, Type.DINOSAUR);
+        }
+    }
+
+    @Test
+    public void Level()
+    {
+        List<String> clist = cardSelector.level(11, Operator.EQ).execute();
+        for (String c : clist) {
+            StaticCardData scd = cardSelector.query(c);
+            assertEquals(scd.level, 11);
+        }
+    }
+
+    @Test
+    public void Attack()
+    {
+        List<String> clist = cardSelector.type(Type.DRAGON).attack(1000,
+                Operator.LT).execute();
+        for (String c : clist) {
+            StaticCardData scd = cardSelector.query(c);
+            assertTrue(scd.attack < 1000);
+        }
+    }
+
+    @Test
+    public void Defense()
+    {
+        List<String> clist = cardSelector.type(Type.ROCK).defense(100,
+                Operator.LTE).execute();
+        for (String c : clist) {
+            StaticCardData scd = cardSelector.query(c);
+            assertTrue(scd.defense <= 100);
+        }
+    }
+
+    @Test
+    public void Pendulum()
+    {
+        List<String> clist = cardSelector.lpendulum(8, Operator.GTE).
+            rpendulum(8, Operator.GTE).execute();
+        for (String c : clist) {
+            StaticCardData scd = cardSelector.query(c);
+            assertTrue(scd.lpendulum >= 8);
+            assertTrue(scd.rpendulum >= 8);
+        }
+    }
+
+    @Test
+    public void SpellType()
+    {
+        List<String> clist = cardSelector.name("Sky").spellType(
+                SpellType.FIELD).execute();
+        for (String c : clist) {
+            StaticCardData scd = cardSelector.query(c);
+            assertEquals(scd.spellType, SpellType.FIELD);
+        }
+    }
+
+    @Test
+    public void TrapType()
+    {
+        List<String> clist = cardSelector.name("Solemn").trapType(
+                TrapType.COUNTER).execute();
+        for (String c : clist) {
+            StaticCardData scd = cardSelector.query(c);
+            assertEquals(scd.trapType, TrapType.COUNTER);
         }
     }
 }
